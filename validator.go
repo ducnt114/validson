041aba294e5d9365db81c0b91fdc661cf35f320a
ducnt114 <trungduc110492@gmail.com>
2020-10-16 00:42:19 +0700

    Update check required fields

--- validator.go
+++ validator.go
@@ -1,8 +1,52 @@
 package validson
 
-import "github.com/ducnt114/validson/models"
+import (
+	"encoding/json"
+	"github.com/ducnt114/validson/models"
+	"io/ioutil"
+	"os"
+)
 
-func Validate(schema *models.Schema, input map[string]interface{}) bool {
-	// TODO
-	return false
+type Validator interface {
+	Validate(input map[string]interface{}) bool
+}
+
+type validatorImpl struct {
+	schema *models.Schema
+}
+
+func NewValidatorWithSchema(schemaFilePath string) (Validator, error) {
+	jsonFile, err := os.Open(schemaFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+
+	bytesVal, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	schema := &models.Schema{}
+	err = json.Unmarshal(bytesVal, &schema)
+	if err != nil {
+		return nil, err
+	}
+
+	return &validatorImpl{
+		schema: schema,
+	}, nil
+}
+
+func (v *validatorImpl) Validate(input map[string]interface{}) bool {
+	// Check required fields
+	for _, requireField := range v.schema.Required {
+		if _, existed := input[requireField]; !existed {
+			return false
+		}
+	}
+
+	// TODO: check properties
+
+	return true
 }
--- validator_test.go
+++ validator_test.go
@@ -1,1 +1,51 @@
 package validson
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestValidate_Required_Success(t *testing.T) {
+	validator, err := NewValidatorWithSchema("./test_data/schemas/schema_1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jsonInput := make(map[string]interface{})
+
+	inputData := `
+{
+    "id": 1,
+    "name": "Lampshade",
+    "price": 9
+}
+`
+	_ = json.Unmarshal([]byte(inputData), &jsonInput)
+
+	res1 := validator.Validate(jsonInput)
+	if res1 == false {
+		t.FailNow()
+	}
+}
+
+func TestValidate_Required_Fail(t *testing.T) {
+	validator, err := NewValidatorWithSchema("./test_data/schemas/schema_1.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jsonInput := make(map[string]interface{})
+
+	inputData := `
+{
+    "id": 1,
+    "price": 9
+}
+`
+	_ = json.Unmarshal([]byte(inputData), &jsonInput)
+
+	res1 := validator.Validate(jsonInput)
+	if res1 == true {
+		t.FailNow()
+	}
+}
